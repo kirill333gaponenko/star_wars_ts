a59f39753e827b6aaa19fc458679438188084adb
kirill <kirill56gaponenko@gmail.com>
2025-12-12 04:22:55 +0200

    Refactor hero validation logic into a custom hook, simplify component logic, and improve code reusability.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,36 @@
 import Navigation from "./Navigation.tsx";
-import {useContext} from "react";
+import {useContext, useEffect} from "react";
 import {SWContext} from "../utils/context.ts";
-import {characters} from "../utils/constants.ts";
+import {characters, defaultHero} from "../utils/constants.ts";
+import {useParams} from "react-router";
+import Text from "./ui/Text.tsx";
 
 const Header = () => {
+    const {changeHero} = useContext(SWContext);
     const {hero} = useContext(SWContext)
+    const {heroId = defaultHero} = useParams();
 
-    return (
+    useEffect(() => {
+        if (!(heroId in characters)) {
+            return;
+        }
+        changeHero(heroId);
+    }, [heroId]);
+    return (heroId in characters)?(
         <header className="rounded-t-3xl bg-grey">
             <Navigation/>
-            <h1 className="text-center text-4xl py-6">{characters[hero].name}</h1>
+            <h1 className="text-center text-4xl py-6">
+                {characters[hero].name}
+            </h1>
         </header>
-    )
+    ):<header className="rounded-t-3xl bg-grey">
+        <Navigation/>
+        <h1 className="text-center text-4xl py-6">
+            <Text>
+                ERROR
+            </Text>
+        </h1>
+    </header>
 }
 
 export default Header;
